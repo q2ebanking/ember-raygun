3ab320cb3e03d725e19c35a3592ff64bc73bffd2
Bill Heaton <pixelhandler@gmail.com>
2017-10-12 09:41:35 -0700

    Upgrade ember-cli to 2.16.2, and ember source to 2.16.0

--- tests/dummy/app/utils/raygun-error-filter.js
+++ tests/dummy/app/utils/raygun-error-filter.js
@@ -1,5 +1,5 @@
 export default function raygunErrorFilter(error) {
-  if (error && error.xhr && error.xhr.status && [401,403,409].includes(error.xhr.status)) {
+  if (error && error.xhr && error.xhr.status && [401,403,409].indexOf(error.xhr.status) > -1) {
     return null;
   } else {
     return error;
